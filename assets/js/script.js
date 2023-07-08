bce4eb73c25ff446d5bf62e728c1ff47e0681566
jamieroche1987 <>
2023-07-08 01:14:07 +0000

    Add js to winning combinations

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,6 +18,10 @@ let count = 0;
 let playerXScore = 0;
 let playerOScore = 0;
 
+//Buttons combinations to win the game
+let winningPosition = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
+
+
 
 
 
