01f6f295bf5f1264571b61176bb7bbb7ff2347a5
jamieroche1987 <>
2023-07-08 00:45:14 +0000

    Add js elements from html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,9 @@
+// Adding all the elements needed from the html
 
+let buttons = document.querySelectorAll(".field"),
+    popupmessageE1 = document.querySelector(".popup-message"),
+    messageE1 = document.querySelector(".message"),
+    newGameBtn = document.querySelector(".new-game"),
+    restartBtn = document.querySelector(".restart"),
+    playerXScoreE1 = document.querySelector(".player-x"),
+    playerOScoreE1 = document.querySelector(".player-o")
