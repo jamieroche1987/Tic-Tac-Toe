27567c9b22a5f61dafb0f6abd9a0dfcb3a4024fd
jamieroche1987 <>
2023-07-10 14:13:43 +0000

    Edit js

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,130 +15,134 @@ let playerXScore = 0;
 let playerOScore = 0;
 //Buttons combinations to win the game
 let winningPosition = [
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8],
-  [0, 4, 8],
-  [2, 4, 6],
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
 ];
 //Event Listener for buttons
 newGameBtn.onclick = function () {
-  enableButtons();
-  determineFirstTurn();
-  count = 0;
+    enableButtons();
+    determineFirstTurn();
+    count = 0;
 };
 restartBtn.onclick = () => {
-  enableButtons();
-  determineFirstTurn();
-  count = 0;
+    enableButtons();
+    determineFirstTurn();
+    count = 0;
+    playerXScore = 0;
+    playerOScore = 0;
+    playerXScoreE1.innerText = playerXScore;
+    playerOScoreE1.innerText = playerOScore;
 };
 //Functions
 function disableButtons() {
-  buttons.forEach((button) => {
-    button.disabled = true;
-  });
-  //Shows pop up message after a certain time
-  setTimeout(() => {
-    popupmessageE1.classList.add("show");
-  }, 500);
+    buttons.forEach((button) => {
+        button.disabled = true;
+    });
+    //Shows pop up message after a certain time
+    setTimeout(() => {
+        popupmessageE1.classList.add("show");
+    }, 500);
 
-  // Removes pop up message
-  popupmessageE1.classList.remove("show");
+    // Removes pop up message
+    popupmessageE1.classList.remove("show");
 }
 
 function enableButtons() {
-  buttons.forEach((button) => {
-    button.disabled = false;
-    button.innerText = "";
-    button.style.backgroundColor = "#FFB739";
-    button.style.color = "rgb(132, 130, 149)";
-  });
-  popupmessageE1.classList.remove("show");
+    buttons.forEach((button) => {
+        button.disabled = false;
+        button.innerText = "";
+        button.style.backgroundColor = "#FFB739";
+        button.style.color = "rgb(132, 130, 149)";
+    });
+    popupmessageE1.classList.remove("show");
 }
 
 function determineFirstTurn() {
-  if (turnFlag === 0) {
-    turnFlag = 1;
-  } else {
-    turnFlag = 0;
-  }
+    if (turnFlag === 0) {
+        turnFlag = 1;
+    } else {
+        turnFlag = 0;
+    }
 }
 //Display winning message
 function wins(button1, button2, button3) {
-  disableButtons();
-  //Sets different color to winning buttons
-  button1.setAttribute("style", "color:blue; background-color:grey");
-  button2.setAttribute("style", "color:blue; background-color:grey");
-  button3.setAttribute("style", "color:blue; background-color:grey");
-  //Display the winner
-  messageE1.innerText = `Player ${button1.innerText} wins`;
+    disableButtons();
+    //Sets different color to winning buttons
+    button1.setAttribute("style", "color:blue; background-color:grey");
+    button2.setAttribute("style", "color:blue; background-color:grey");
+    button3.setAttribute("style", "color:blue; background-color:grey");
+    //Display the winner
+    messageE1.innerText = `Player ${button1.innerText} wins`;
 }
 //Display the draw message and disable the buttons
 function draw() {
-  disableButtons();
-  messageE1.innerText = "Game Drawn";
+    disableButtons();
+    messageE1.innerText = "Game Drawn";
 }
 //Update the Score
 function updateScore(value) {
-  if (value == "X") {
-    playerXScore++;
-    playerXScoreE1.innerText = playerXScore;
-  } else if (value == "O") {
-    playerOScore++;
-    playerOScoreE1.innerText = playerOScore;
-  }
+    if (value == "X") {
+        playerXScore++;
+        playerXScoreE1.innerText = playerXScore;
+    } else if (value == "O") {
+        playerOScore++;
+        playerOScoreE1.innerText = playerOScore;
+    }
 }
 
 //Check for the winner
 function checkwins() {
-  for (let i = 0; i < winningPosition.length; i++) {
-    let [element1, element2, element3] = [
-      buttons[winningPosition[i][0]],
-      buttons[winningPosition[i][1]],
-      buttons[winningPosition[i][2]],
-    ];
-    //Check if the elements above are filled in (either with and X or O)
-    if (
-      element1.innerText != "" &&
-      element2.innerText != "" &&
-      element3.innerText != ""
-    ) {
-      //Check elements have the same value or not
-      if (
-        element1.innerText == element2.innerText &&
-        element2.innerText == element3.innerText &&
-        element3.innerText == element1.innerText
-      ) {
-        updateScore(element1.innerText);
-        disableButtons();
-        wins(element1, element2, element3);
-      }
+    for (let i = 0; i < winningPosition.length; i++) {
+        let [element1, element2, element3] = [
+            buttons[winningPosition[i][0]],
+            buttons[winningPosition[i][1]],
+            buttons[winningPosition[i][2]],
+        ];
+        //Check if the elements above are filled in (either with and X or O)
+        if (
+            element1.innerText != "" &&
+            element2.innerText != "" &&
+            element3.innerText != ""
+        ) {
+            //Check elements have the same value or not
+            if (
+                element1.innerText == element2.innerText &&
+                element2.innerText == element3.innerText &&
+                element3.innerText == element1.innerText
+            ) {
+                updateScore(element1.innerText);
+                disableButtons();
+                wins(element1, element2, element3);
+            }
+        }
     }
-  }
 }
 //Write X or O on the button clicked
 buttons.forEach((button) => {
-  button.onclick = function () {
-    if (turnFlag === 0) {
-      button.innerText = "O";
-      console.log("O ->" + button.innerText);
-      button.disabled = true;
-      turnFlag = 1;
-    } else {
-      button.innerText = "X";
-      console.log("X ->" + button.innerText);
-      button.disabled = true;
-      turnFlag = 0;
-    }
-    count++;
-    if (count === 9) {
-      draw();
-    }
-    checkwins();
-  };
+    button.onclick = function () {
+        if (turnFlag === 0) {
+            button.innerText = "O";
+            console.log("O ->" + button.innerText);
+            button.disabled = true;
+            turnFlag = 1;
+        } else {
+            button.innerText = "X";
+            console.log("X ->" + button.innerText);
+            button.disabled = true;
+            turnFlag = 0;
+        }
+        count++;
+        if (count === 9) {
+            draw();
+        }
+        checkwins();
+    };
 });
 
 updateScore();
